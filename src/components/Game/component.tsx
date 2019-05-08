7313b1c9ec418481e6438f1c3ec41b2d97f4d831
RoboPhred <robophred@gmail.com>
2019-05-08 10:49:34 -0700

    Extract hook for drag behavior

--- src/components/Game/component.tsx
+++ src/components/Game/component.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import useComponentSize from "@rehooks/component-size";
+import injectSheets from "react-jss";
+
+import { createStyles, WithStyles } from "@/theme";
 
 import { Vector2, VEC_ZERO, magnitude, subtract } from "@/math";
+
+import usePointerDrag, { DragEvent } from "@/hooks/use-pointer-drag";
 
 import BallField from "../BallField";
 import BouncerField from "../BouncerField";
@@ -14,51 +19,32 @@ export interface GameProps {
   onResize(x: number, y: number): void;
 }
 
-const Game: React.FC<GameProps> = ({
+const styles = createStyles({
+  svg: {
+    touchAction: "none"
+  }
+});
+
+type Props = GameProps & WithStyles<typeof styles>;
+const Game: React.FC<Props> = ({
   className,
+  classes,
   onCreateBouncer,
   onResize
 }) => {
-  const [newBouncerStart, setNewBouncerStart] = React.useState<Vector2 | null>(
-    null
-  );
-  const [newBouncerEnd, setNewBouncerEnd] = React.useState<Vector2 | null>(
-    null
-  );
-  const pointerDown = React.useCallback(
-    (e: React.PointerEvent<SVGElement>) => {
-      setNewBouncerStart({ x: e.clientX, y: e.clientY });
-      setNewBouncerEnd({ x: e.clientX, y: e.clientY });
-      e.stopPropagation();
-      e.preventDefault();
-    },
-    [setNewBouncerStart, setNewBouncerEnd]
-  );
-  const pointerMove = React.useCallback(
-    (e: React.PointerEvent<SVGElement>) => {
-      setNewBouncerEnd({ x: e.clientX, y: e.clientY });
-    },
-    [setNewBouncerEnd]
-  );
-  const pointerUp = React.useCallback(
-    (e: React.PointerEvent<SVGElement>) => {
-      if (newBouncerStart == null) {
-        return;
-      }
-      const newBouncerFinal = { x: e.clientX, y: e.clientY };
-      const length = Math.abs(
-        magnitude(subtract(newBouncerStart, newBouncerFinal))
-      );
+  const createBouncerDragRelease = React.useCallback(
+    (e: DragEvent) => {
+      const start = { x: e.start.clientX, y: e.start.clientY };
+      const end = { x: e.clientX, y: e.clientY };
+      const length = Math.abs(magnitude(subtract(start, end)));
       if (length >= 50) {
-        onCreateBouncer(newBouncerStart, newBouncerFinal);
+        onCreateBouncer(start, end);
       }
-      setNewBouncerStart(null);
-      setNewBouncerEnd(null);
-      e.stopPropagation();
-      e.preventDefault();
     },
-    [newBouncerStart, setNewBouncerStart, setNewBouncerEnd, onCreateBouncer]
+    [onCreateBouncer]
   );
+
+  const useBouncerPointer = usePointerDrag(createBouncerDragRelease);
 
   const ref = React.useRef(null);
   const [oldSize, setOldSize] = React.useState(VEC_ZERO);
@@ -71,23 +57,24 @@ const Game: React.FC<GameProps> = ({
   return (
     <div className={className} ref={ref}>
       <svg
+        className={classes.svg}
         width={`${size.width}`}
         height={`${size.height}`}
         viewBox={`0 0 ${size.width} ${size.height}`}
-        onPointerDown={pointerDown}
-        onPointerMove={pointerMove}
-        onPointerUp={pointerUp}
+        onPointerDown={useBouncerPointer.pointerDown}
+        onPointerMove={useBouncerPointer.pointerMove}
+        onPointerUp={useBouncerPointer.pointerUp}
       >
         <GravitySlider r={15} cx={size.width - 35} cy={size.height - 35} />
         <BallField />
         <BouncerField />
         <EmitterField />
-        {newBouncerStart && newBouncerEnd && (
+        {useBouncerPointer.start && useBouncerPointer.end && (
           <line
-            x1={newBouncerStart.x}
-            y1={newBouncerStart.y}
-            x2={newBouncerEnd.x}
-            y2={newBouncerEnd.y}
+            x1={useBouncerPointer.start.clientX}
+            y1={useBouncerPointer.start.clientY}
+            x2={useBouncerPointer.end.clientX}
+            y2={useBouncerPointer.end.clientY}
             stroke="darkgrey"
           />
         )}
@@ -95,4 +82,4 @@ const Game: React.FC<GameProps> = ({
     </div>
   );
 };
-export default Game;
+export default injectSheets(styles)(Game);
---
+++ src/hooks/use-pointer-drag.ts
@@ -0,0 +1,101 @@
+import * as React from "react";
+
+import { Vector2 } from "@/math";
+
+export interface ClientPos {
+  clientX: number;
+  clientY: number;
+}
+
+export interface DragEvent {
+  clientX: number;
+  clientY: number;
+  start: ClientPos;
+  dX: number;
+  dY: number;
+}
+
+export interface UsePointerEvents {
+  start: ClientPos | null;
+  end: ClientPos | null;
+  pointerDown(e: React.PointerEvent<Element>): void;
+  pointerMove(e: React.PointerEvent<Element>): void;
+  pointerUp(e: React.PointerEvent<Element>): void;
+}
+
+export default function usePointerDrag(
+  onDragComplete: (e: DragEvent) => void,
+  onMove?: (e: DragEvent) => void
+) {
+  const [mouseDown, setMouseDown] = React.useState<Vector2 | null>(null);
+  const [mouseMove, setMouseMove] = React.useState<Vector2 | null>(null);
+
+  const pointerDown = React.useCallback((e: React.PointerEvent<Element>) => {
+    setMouseDown({ x: e.clientX, y: e.clientY });
+    e.currentTarget.setPointerCapture(e.pointerId);
+
+    e.preventDefault();
+    e.stopPropagation();
+  }, []);
+
+  const pointerMove = React.useCallback(
+    (e: React.PointerEvent<Element>) => {
+      if (!mouseDown) {
+        return;
+      }
+
+      if (onMove) {
+        const ev: DragEvent = {
+          start: {
+            clientX: mouseDown.x,
+            clientY: mouseDown.y
+          },
+          clientX: e.clientX,
+          clientY: e.clientY,
+          dX: e.clientX - mouseDown.x,
+          dY: e.clientY - mouseDown.y
+        };
+        onMove(ev);
+      }
+
+      setMouseMove({ x: e.clientX, y: e.clientY });
+
+      e.preventDefault();
+    },
+    [mouseDown, onMove]
+  );
+
+  const pointerUp = React.useCallback(
+    (e: React.PointerEvent<Element>) => {
+      e.currentTarget.releasePointerCapture(e.pointerId);
+
+      if (mouseDown) {
+        const ev: DragEvent = {
+          start: {
+            clientX: mouseDown.x,
+            clientY: mouseDown.y
+          },
+          clientX: e.clientX,
+          clientY: e.clientY,
+          dX: e.clientX - mouseDown.x,
+          dY: e.clientY - mouseDown.y
+        };
+        onDragComplete(ev);
+
+        setMouseDown(null);
+
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    },
+    [onDragComplete, mouseDown]
+  );
+
+  return {
+    start: mouseDown ? { clientX: mouseDown.x, clientY: mouseDown.y } : null,
+    end: mouseMove ? { clientX: mouseMove.x, clientY: mouseMove.y } : null,
+    pointerDown,
+    pointerMove,
+    pointerUp
+  };
+}
